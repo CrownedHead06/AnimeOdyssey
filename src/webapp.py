04604b4ec0d3dbbf44b9ee7ec461f7a669cd6a38
Tech Titan <techtitan1406@gmail.com>
2023-07-05 22:45:53 +0530

    solved some bugs in webapp.py

--- src/webapp.py
+++ src/webapp.py
@@ -6,7 +6,6 @@ from pathlib import Path
 # importing helper files
 from aseries import recommendSeries
 from amovies import recommendMovies
-from prompt import recommendAnimes
 
 # page configuration
 st.set_page_config(
@@ -19,13 +18,6 @@ st.set_page_config(
 logo = Image.open(Path.cwd()/'assets/pictures/logo.png')
 st.sidebar.image(logo, caption='ANIME ODYSSEY')
 
-# choose a option
-choice = st.sidebar.radio(
-    'How would you like to get recommended: 🧐',
-    ['By name of a Series/Movie', 
-     'By Describing an anime in your words']
-)
-
 # What is Anime Odyssey?
 description = """Anime Odyssey is a community originally formed by a passionate otaku who shared his love for anime across various social media platforms. Over time, he recognized a common dilemma among his fellow members - the struggle to find new anime series or movies to watch after finishing their current favorites. To address this issue, he took a bold step and created an anime recommender system.
 
@@ -35,41 +27,29 @@ st.sidebar.title('What is Anime Odyssey? 🥷')
 st.sidebar.write(description)
 
 
-# if user chose '0' option
-if choice == 'By name of a Series/Movie':
-    # page header
-    st.title('Anime Odyssey: Embark on a Journey of Recommendations 🥷')
+st.title('Anime Odyssey: Embark on a Journey of Recommendations 🥷')
 
-    # divide the section into two columns
-    col1, col2 = st.columns(2)
+# divide the section into two columns
+col1, col2 = st.columns(2)
 
-    # select one
-    with col1:
-        option = st.selectbox('Select Anime Type: 👇', ['Anime Series', 'Anime Movies'])
+# select one
+with col1:
+    option = st.selectbox('Select Anime Type: 👇', ['Anime Series', 'Anime Movies'])
 
-    # now time to start suggesting
-    if option == 'Anime Series':
-        try:
-            recommendSeries()
-        except:
-            pass
-    if option == 'Anime Movies':
-        try:
-            recommendMovies()
-        except:
-            pass
+# now time to start suggesting
+if option == 'Anime Series':
+    try:
+        recommendSeries()
+    except:
+        pass
+if option == 'Anime Movies':
+    try:
+        recommendMovies()
+    except:
+        pass
 
-
-# if user chose '1' option
-if choice == 'By Describing an anime in your words':
-    # page header
-    st.title('Anime Odyssey: Embark on a Journey of Recommendations 🥷')
-
-    # catch the error if you can
-    recommendAnimes() 
-
-    # copyright section
-    st.markdown('# ')
-    st.markdown('# ')
-    copyright = "&copy; 2023 Tech Titans Inc. All rights reserved."
-    st.markdown(f'<p style="text-align:center;">{copyright}</p>', unsafe_allow_html=True)
+# copyright section
+st.markdown('# ')
+st.markdown('# ')
+copyright = "&copy; 2023 Tech Titans Inc. All rights reserved."
+st.markdown(f'<p style="text-align:center;">{copyright}</p>', unsafe_allow_html=True)
