f35c9c12fe894feaa90595e68739450645bced09
Tech Titan <techtitan1406@gmail.com>
2023-07-12 12:24:58 +0530

    updated binary file path in prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -6,9 +6,13 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# read binary and dataset 
-animes = pd.read_csv(Path.cwd()/'datasets/anime_series.csv')
-vectors = pickle.load(open(Path.cwd()/'assets/bin/vectors.pkl', 'rb'))
+# read and merge dataset 
+series = pd.read_csv(Path.cwd()/'datasets/anime_series.csv')
+movies = pd.read_csv(Path.cwd()/'datasets/anime_movies.csv')
+animes = series.append(movies)
+
+# read binary
+vectors = pickle.load(open(Path.cwd()/'assets/bin/dembeddings.pkl', 'rb'))
 
 # making cosine_similarity function
 def cosine_similarity(embeddings, vectors):
@@ -44,18 +48,21 @@ def recommendAnimes() -> None:
     # time to display best match according to prompt
     if st.button('Recommend'):
         for i in distances[0:15]:
-            title = animes.iloc[i[0]].title
-            poster = animes.iloc[i[0]].poster
-            description = animes.iloc[i[0]].description
-            url = animes.iloc[i[0]].link
+            try:
+                title = animes.iloc[i[0]].title
+                poster = animes.iloc[i[0]].poster
+                description = animes.iloc[i[0]].description
+                url = animes.iloc[i[0]].link
 
-            # divide the page into 2 columns
-            col1, col2 = st.columns(2)
+                # divide the page into 2 columns
+                col1, col2 = st.columns(2)
 
-            # print the attributes on the page
-            with col1:
-                st.image(poster, width=200)
-            with col2:
-                styled_title = f'<a href="{url}" style="color:red; font-size:1.2em;">{title}</a>'
-                st.markdown(styled_title, unsafe_allow_html=True)
-                st.write(description[0:400] + ' ...')
+                # print the attributes on the page
+                with col1:
+                    st.image(poster, width=200)
+                with col2:
+                    styled_title = f'<a href="{url}" style="color:red; font-size:1.2em;">{title}</a>'
+                    st.markdown(styled_title, unsafe_allow_html=True)
+                    st.write(description[0:400] + ' ...')
+            except:
+                continue
